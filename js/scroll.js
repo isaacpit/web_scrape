70d7bbd704d9f6b9485d0d29b29fd9d332c2a371
isaacpitblado <isaacpitblado@tamu.edu>
2019-08-21 13:48:15 -0500

    added timeout for delaying downloads

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,29 @@
-const puppeteer = require('puppeteer');
+var puppeteer = require('puppeteer');
+var fs = require('fs');
+var request = require('request');
+
+const axios = require('axios');
+var download = require('./download');
+
+
+/* ============================================================
+  Function: Download Image
+============================================================ */
+
+const download_image = (url, image_path) =>
+  axios({
+    url,
+    responseType: 'stream',
+  }).then(
+    response =>
+      new Promise((resolve, reject) => {
+        response.data
+          .pipe(fs.createWriteStream(image_path))
+          .on('finish', () => resolve())
+          .on('error', e => reject(e));
+      }),
+  );
+
 
 async function getMoviesIds(reqData) {
   const browser = await puppeteer.launch({
@@ -53,12 +78,44 @@ function cleanLinks(hrefs, search_term, search_end) {
   return arr_clean_links;
 }
 
-async function downloadMovies(movieData) {
-  console.log("working on " + movieData.lst_movie_ids.length + " download links: " );
+async function getDownloadLinks(movieData) {
+  // console.log("working on " + movieData.lst_movie_ids.length + " download links: " );
+  var download_links = [];
   for (var i = 0; i < movieData.lst_movie_ids.length; ++i) {
     var download_link = movieData.download_link + movieData.lst_movie_ids[i] + "/" + movieData.width + "x" + movieData.height;
-     console.log(download_link);
+    //  console.log(download_link);
+     download_links.push(download_link);
   }
+
+  return download_links;
+}
+
+async function localDownloadFromLinks(lst_download_links) {
+  var folder = "./images/";
+  var suffix = ".jpg";
+  for (var i = 0; i < 3; ++i) {
+  // for (var i = 0; i < lst_download_links.length; ++i) {
+    // const example_image_1 = await download_image(lst_download_links[i], folder + i + ".jpg");
+    console.log("i " + i + ": " + lst_download_links[i]);
+
+    await sleep_delay_download(1000);
+    setTimeout(download, 2000 ,lst_download_links[i], 
+      folder + i + suffix, function (state) {
+      console.log("progress", state);
+    }, function (response) {
+        console.log("status code", response.statusCode);
+    }, function (error) {
+        console.log("error", error);
+    }, function () {
+        console.log("done");
+    });
+  }
+
+  return "poopoo";
+}
+
+function sleep_delay_download(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 class ReqData {
@@ -119,13 +176,37 @@ var reqData = new ReqData(
   HEIGHT
 );
 
+
+
+// var download = function(uri, filename, callback){
+//   request.head(uri, function(err, res, body){
+//     console.log('content-type:', res.headers['content-type']);
+//     console.log('content-length:', res.headers['content-length']);
+
+//     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+//   });
+// };
+
+
+
+
+
 movies = getMoviesIds(reqData)
   .then(function(lst_movie_ids) {
-    console.log("after: ");
+    console.log("after getting movie ids... ");
     console.log(lst_movie_ids);
     
     var movieData = new MovieData(lst_movie_ids, DOWNLOAD_LINK, WIDTH, HEIGHT);
-    downloadMovies(movieData);
+    var lst_download_links = getDownloadLinks(movieData).then(function(lst_download_links) {
+      console.log("after getting download links... ");
+      console.log(lst_download_links);
+
+      var returned_from_download = localDownloadFromLinks(lst_download_links).then(function(returned_obj) {
+        
+        console.log(returned_obj);
+      });
+    });
+    
     
   });
 
